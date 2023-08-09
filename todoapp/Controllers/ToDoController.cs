1ec138e1d93bbff4cc58420067b51b0459f47424
Goonja <A00277328@mycambrian.ca>
2023-08-08 22:23:40 -0400

    added comments
    comments added in all files

--- todoapp/Controllers/ToDoController.cs
+++ todoapp/Controllers/ToDoController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Text.Json;
-using Microsoft.AspNetCore.Http;
 using todoapp.Models;
-using System.Diagnostics; // Add this using directive for Debug
 
 namespace todoapp.Controllers
 {
@@ -19,62 +16,60 @@ namespace todoapp.Controllers
             _logger = logger;
         }
 
+        // Retrieves the to-do list from TempData or initializes a new list if empty
         private List<ToDoModel> InitializeToDoList()
         {
             var todoListJson = TempData.Peek(TempDataKey) as string;
+
             if (string.IsNullOrEmpty(todoListJson))
             {
-                Console.WriteLine("data empty entered");
                 return new List<ToDoModel>
                 {
                     new ToDoModel { Task = "Do Web assignment", Id = 0 },
-                    new ToDoModel {  Task = "Do iOS assignment", Id = 1 },
+                    new ToDoModel { Task = "Do iOS assignment", Id = 1 },
                     new ToDoModel { Task = "Learn new chords", Id = 2 },
-                    new ToDoModel {Task = "Get new Guitar bag",Id = 3 }
+                    new ToDoModel { Task = "Get new Guitar bag", Id = 3 }
                 };
             }
+
             return JsonSerializer.Deserialize<List<ToDoModel>>(todoListJson);
         }
 
+        // Saves the to-do list into TempData as JSON data
         private void SaveToDoList(List<ToDoModel> todoList)
         {
-            var todoListJson = JsonSerializer.Serialize(todoList);
-            TempData[TempDataKey] = todoListJson;
+            TempData[TempDataKey] = JsonSerializer.Serialize(todoList);
         }
 
+        // Displays the to-do list on the index page
         public IActionResult Index()
         {
             var todoList = InitializeToDoList();
-            Console.WriteLine(todoList[3].Id);
             return View(todoList);
         }
 
+        // Adds a new task to the to-do list and redirects to the index page
         [HttpPost]
         public IActionResult AddTask(string Task)
         {
             var todoList = InitializeToDoList();
-            todoList.Add(new ToDoModel { Task = Task , Id = todoList.Count});
-
+            todoList.Add(new ToDoModel { Task = Task, Id = todoList.Count });
             SaveToDoList(todoList);
-
-            return RedirectToAction("Index"); // Redirect to Index action to prevent form resubmission
+            return RedirectToAction("Index");
         }
 
+        // Toggles the completion status of a task in the to-do list and redirects to the index page
         [HttpPost]
         public IActionResult ToggleTask(int ToDoIndex)
         {
             var todoList = InitializeToDoList();
-            Console.WriteLine(ToDoIndex);
-             Console.WriteLine(todoList[ToDoIndex].Task);
+
             foreach (var task in todoList)
             {
-                Console.WriteLine($"{task.Task} {task.Id}");
                 if (task.Id == ToDoIndex)
                 {
-                    Console.WriteLine($"Before Toggle - Task Id: {task.Id}, Task Done: {task.TaskDone}, {task.Task}");
                     task.TaskDone = !task.TaskDone;
                     SaveToDoList(todoList);
-                    Console.WriteLine($"After Toggle - Task Id: {task.Id}, Task Done: {task.TaskDone}, {task.Task}");
                     break; // Exit the loop once the task is found and updated
                 }
             }
--- todoapp/Models/ToDoModel.cs
+++ todoapp/Models/ToDoModel.cs
@@ -1,12 +1,17 @@
 using System; 
 
-namespace todoapp.Models {
-
-    public class ToDoModel {
-
+namespace todoapp.Models 
+{
+    // Represents a single task in the to-do list
+    public class ToDoModel 
+    {
+        // Gets or sets the unique identifier of the task
         public int Id { get; set; }
+        
+        // Gets or sets the description of the task
         public string? Task { get; set; }
+        
+        // Gets or sets a value indicating whether the task is marked as done or not
         public bool TaskDone { get; set; }
     }
-
 }
